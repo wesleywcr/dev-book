853d08d4a7e6025d87cfbcb28aa96de2f13cc052
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2025-03-24 23:31:00 -0300

    feat: add routes users

---
+++ api/router/users.go
@@ -0,0 +1,40 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/wesleywcr/dev-book/api/controllers"
+)
+
+var routesUsers = []Route{
+	{
+		URI:                   "/users",
+		Method:                http.MethodPost,
+		HandleFunction:        controllers.CreateUser,
+		RequiredAuthorization: false,
+	},
+	{
+		URI:                   "/users",
+		Method:                http.MethodGet,
+		HandleFunction:        controllers.ListUsers,
+		RequiredAuthorization: false,
+	},
+	{
+		URI:                   "/users/${userId}",
+		Method:                http.MethodGet,
+		HandleFunction:        controllers.ListUser,
+		RequiredAuthorization: false,
+	},
+	{
+		URI:                   "/users/${userId}",
+		Method:                http.MethodPut,
+		HandleFunction:        controllers.UpdateUser,
+		RequiredAuthorization: false,
+	},
+	{
+		URI:                   "/users/${userId}",
+		Method:                http.MethodDelete,
+		HandleFunction:        controllers.DeleteUser,
+		RequiredAuthorization: false,
+	},
+}
