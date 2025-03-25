db3e388e2c9889de28eb3e9ee819e2891a8fc6f5
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2025-03-25 14:28:59 -0300

    feat: add validations and insert user in DB

--- api/controllers/users.go
+++ api/controllers/users.go
@@ -3,21 +3,48 @@ package controllers
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 
+	"github.com/wesleywcr/dev-book/api/db"
 	"github.com/wesleywcr/dev-book/api/models"
+	"github.com/wesleywcr/dev-book/api/repositories"
+	"github.com/wesleywcr/dev-book/api/response"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	bodyRequest, error := io.ReadAll(r.Body)
+
 	if error != nil {
-		log.Fatal(error)
+		response.Error(w, http.StatusUnprocessableEntity, error)
+		return
 	}
+
 	var user models.User
 	if error = json.Unmarshal(bodyRequest, &user); error != nil {
-		log.Fatal((error))
+		response.Error(w, http.StatusBadRequest, error)
+		return
 	}
+	if error = user.Prepare(); error != nil {
+		response.Error(w, http.StatusBadRequest, error)
+		return
+	}
+
+	//connect DB
+	db, error := db.ConnectDB()
+	if error != nil {
+		response.Error(w, http.StatusInternalServerError, error)
+		return
+	}
+
+	defer db.Close()
+	// insert in DB
+	repository := repositories.NewRepositoryOfUsers(db)
+	user.ID, error = repository.Create(user)
+	if error != nil {
+		response.Error(w, http.StatusInternalServerError, error)
+		return
+	}
+	response.JSON(w, http.StatusCreated, user)
 
 }
 func ListUsers(w http.ResponseWriter, r *http.Request) {
