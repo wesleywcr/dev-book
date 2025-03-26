5639907a17689123d3c5b12f060163eb3f99ecbd
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2025-03-26 10:37:12 -0300

    feat: add pkg auth and validate token jwt

--- api/auth/token.go
+++ api/auth/token.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -15,4 +19,34 @@ func CreateToken(userID uint64) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permitions)
 	return token.SignedString([]byte(config.SecretKey))
+}
+
+func ValidateToken(r *http.Request) error {
+	tokenString := extractToken(r)
+	token, error := jwt.Parse(tokenString, returnVerificationKey)
+	if error != nil {
+		return error
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+	return errors.New("Token inválido!")
+
+}
+
+func extractToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+	return ""
+}
+
+func returnVerificationKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Método de assinatura inesperado! %v", token.Header["alg"])
+	}
+	return config.SecretKey, nil
 }
