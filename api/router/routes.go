23c80f5fc96d95adcf48efdb5ca4caa64c118ddf
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2025-03-26 10:37:57 -0300

    feat: add middlewres

--- api/router/routes.go
+++ api/router/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/wesleywcr/dev-book/api/middlewares"
 )
 
 type Route struct {
@@ -15,8 +16,15 @@ type Route struct {
 
 func Config(r *mux.Router) *mux.Router {
 	routes := routesUsers
+	routes = append(routes, routeLogin)
+
 	for _, route := range routes {
-		r.HandleFunc(route.URI, route.HandleFunction).Methods(route.Method)
+		if route.RequiredAuthorization {
+			r.HandleFunc(route.URI, middlewares.Logger(middlewares.Authenticate(route.HandleFunction))).Methods(route.Method)
+		} else {
+			r.HandleFunc(route.URI, middlewares.Logger(route.HandleFunction)).Methods(route.Method)
+		}
 	}
+
 	return r
 }
